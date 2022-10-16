1304ee58832cd8b393aa18bf3a04b03d94152bd4
Junior Corales <jjcmjavascript@gmail.com>
2022-10-16 00:52:50 -0300

    feat: removed restricted mode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,5 @@ import './app/styles/app.css';
 const rootElement = document.getElementById('root'); 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
